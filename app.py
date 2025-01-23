114a02509473e060f937c7346fe48b4a5e4f37e8
Eric <campagnollo2002@gmail.com>
2025-01-23 09:22:47 -0500

    changed starter.py to app.py and cleaned up the Flask app.

---
+++ app.py
@@ -0,0 +1,68 @@
+"""
+Flask app for web services.
+
+"""
+
+from flask import Flask, render_template
+
+
+
+
+app = Flask(__name__)
+app.secret_key = "password"
+
+
+@app.route('/')
+def home():
+    """
+    Renders the home page.
+
+    Returns:
+        str: The rendered template for the home page.
+    """
+    return render_template('home.html')
+
+@app.route('/story')
+def story():
+    """
+    Renders the story page.
+
+    Returns:
+        str: The rendered template for the story page.
+    """
+    return render_template('story.html')
+
+@app.route('/blog')
+def blog():
+    """
+    Renders the blog page.
+
+    Returns:
+        str: The rendered template for the blog page.
+    """
+    return render_template('blog.html')
+
+@app.route('/forum')
+def forum():
+    """
+    Renders the forum page.
+
+    Returns:
+        str: The rendered template for the forum page.
+    """
+    return render_template('forum.html')
+
+@app.route('/contact')
+def contact():
+    """
+    Renders the contact page.
+
+    Returns:
+        str: The rendered template for the contact page.
+    """
+    return render_template('contact.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
